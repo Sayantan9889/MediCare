9fe490d53a64b636eb936cf909df74afed41f3b2
Sayantan9889 <sayantan.silda98@gmail.com>
2024-02-02 01:40:46 +0530

    Changes

--- src/app/Services/AuthServices/auth.service.ts
+++ src/app/Services/AuthServices/auth.service.ts
@@ -13,27 +13,27 @@ export class AuthService {
   login_api = "https://wtsacademy.dedicateddevelopers.us/api/user/signin";
   profile_api = "https://wtsacademy.dedicateddevelopers.us/api/user/profile-details";
 
-  constructor(private http:HttpClient, private storage:StorageService) {}
+  constructor(private http: HttpClient, private storage: StorageService) { }
 
-  login_user(data: any):Observable<Login[]> {
+  login_user(data: any): Observable<Login[]> {
     return this.http.post<Login[]>(this.login_api, data).pipe(catchError(this.errHandler));
   }
 
-  profile_details():Observable<Registration[]> {
+  profile_details(): Observable<Registration[]> {
     // return this.http.get<Registration[]>(this.profile_api, {
-    //     headers: new HttpHeaders({
-    //       'x-access-token': `${this.storage.getToken()}`
-    //     })
-    //   });
+    //   headers: new HttpHeaders({
+    //     'x-access-token': `${this.storage.getToken()}`
+    //   })
+    // });
 
     return this.http.get<Registration[]>(this.profile_api).pipe(catchError(this.errHandler));
   }
 
-  register_user(data:any):Observable<Registration[]> {
+  register_user(data: any): Observable<Registration[]> {
     return this.http.post<Registration[]>(this.registration_api, data).pipe(catchError(this.errHandler));
   }
 
-  errHandler(error:HttpErrorResponse) {
+  errHandler(error: HttpErrorResponse) {
     return throwError(() => error || "Server Error!");
   }
 }
